69b7a9713ab781e7fc987637dd89f7edc6f61cab
Mykhailov Pavel <mykhailovpm@gmail.com>
2018-03-18 14:36:32 +0700

    starting project

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-extra-semi': 1
+  },
+  globals: {}
+}
