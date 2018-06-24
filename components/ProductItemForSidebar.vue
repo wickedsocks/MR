739de4ea8ba218367a219e79ea27b0ff63fb23a7
wickedsocks <mykhailovpm@gmail.com>
2018-06-24 18:08:53 +0300

    add user friendly delete bucket field

--- components/ProductItemForSidebar.vue
+++ components/ProductItemForSidebar.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="d-flex w-100">
-    <div class="header-cart-item-img" @click="deleteItemFromBucket()">
+    <div class="header-cart-item-img pointer-none">
       <img :src="item.product.images[0]" :alt="item.product.description">
-      <i class="zmdi zmdi-close delete-item-icon"></i>
     </div>
 
     <div class="header-cart-item-txt p-t-8 d-flex flex-column justify-content-between">
       <nuxt-link @click.native="close()" :to="`/product/${item.product._id}`" class="header-cart-item-name hov-cl1 trans-04">
         {{item.product.title}}
       </nuxt-link>
-
-      <span class="header-cart-item-info">
-        {{item.quantity}} x {{item.product.price}} грн
-      </span>
+      <div class="d-flex">
+        <span class="header-cart-item-info">
+          {{item.quantity}} x {{item.product.price}} грн
+        </span>
+        <span class="delete-word header-cart-item-info pointer" @click="deleteItemFromBucket()">
+          удалить
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -22,37 +25,23 @@ export default {
   props: ["item"],
   methods: {
     close() {
-      this.$emit('close');
+      this.$emit("close");
     },
     deleteItemFromBucket() {
-      this.$emit('deleteIndex');
+      this.$emit("deleteIndex");
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.delete-item-icon {
-  color: #ffffff;
-  background-color: rgba(70, 70, 70, 0.56863);
-  font-size: 24px;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  margin: auto;
-  left: 0;
-  right: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  opacity: 0;
-  transition: opacity 0.3s;
-}
-.header-cart-item-img {
+.delete-word {
+  display: inline-block;
+  margin-left: 20px;
+  text-transform: capitalize;
   &:hover {
-    & > .delete-item-icon {
-      opacity: 1;
-    }
+    text-decoration: underline;
+    color: #717fe0;
   }
 }
 </style>
