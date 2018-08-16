d3fa72efdc3c3dfa5d40993b35f9ae6fdac45f07
wickedsocks <mykhailovpm@gmail.com>
2018-08-17 00:06:25 +0300

    remove icon-close2

--- components/Header.vue
+++ components/Header.vue
@@ -106,9 +106,9 @@
       <!-- Modal Search -->
       <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
         <div class="container-search-header">
-          <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+          <!-- <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
             <img src="img/icons/icon-close2.png" alt="CLOSE">
-          </button>
+          </button> -->
 
           <form class="wrap-search-header flex-w p-l-15">
             <button class="flex-c-m trans-04">
