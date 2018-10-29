3ae8c1b321afb302a4b2208ad8fd26c8f8f71830
wickedsocks <mykhailovpm@gmail.com>
2018-10-29 16:15:34 +0200

    remove post methods for manucature and product category

--- server/api/category.js
+++ server/api/category.js
@@ -9,43 +9,6 @@ const {Product} = require('../../models/product');
 
 const router = Router();
 
-router.post('/categories/manufacture', (req, res) => {
-  let trimmedName = req.body.name.trim();
-  let newCategory = new CategoryManufacture({
-    description: req.body.description,
-    name: trimmedName,
-    url: Product.productUrlNaming(req.body.name),
-    used: false
-  });
-
-  newCategory.save().then(
-    success => {
-      res.send(success);
-    },
-    err => {
-      res.status(400).send(err);
-    }
-  );
-});
-
-router.post('/categories/product', (req, res) => {
-  let trimmedName = req.body.name.trim();
-  let newProductCategory = new CategoryProduct({
-    description: req.body.description,
-    name: trimmedName,
-    url: Product.productUrlNaming(req.body.name),
-    used: false
-  });
-  newProductCategory.save().then(
-    success => {
-      res.send(success);
-    },
-    err => {
-      res.status(400).send(err);
-    }
-  );
-});
-
 router.get('/categories/manufacture', (req, res) => {
   CategoryManufacture.find({}).then(
     categories => {
@@ -66,6 +29,32 @@ router.get('/categories/product', (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+router.post('/categories', (req, res) => {
+  let trimmedName = req.body.name.trim();
+  let Category = new Category({
+    description: req.body.description,
+    name: trimmedName,
+    url: Product.productUrlNaming(req.body.name),
+    used: false
+  });
+  Category.save().then(
+    success => {
+      res.send(success);
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
+});
+
+router.get('/categories', (req, res) => {
+ Category.find({}).then((categories) => {
+  res.send(categories);
+ }, (err) => {
+  res.status(400).send(err);
+ })
 });
 
 router.get('/merge-categoires', (req, res) => {
