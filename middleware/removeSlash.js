25dea1ef4acf6bece64d402f98a84f3dadcafaf0
wickedsocks <mykhailovpm@gmail.com>
2018-12-30 14:26:02 +0200

    add 301 redirect

--- middleware/removeSlash.js
+++ middleware/removeSlash.js
@@ -2,6 +2,6 @@ export default function({ route, redirect }) {
   let path = route.path;
   let lastPathElementIndex = path.length - 1;
   if (path[lastPathElementIndex] === '/') {
-    return redirect(path.slice(0, lastPathElementIndex));
+    return redirect(301, path.slice(0, lastPathElementIndex));
   }
 }
