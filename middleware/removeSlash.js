c29a8fc119484da3eccd9a2a379b9bebf872ba2e
wickedsocks <mykhailovpm@gmail.com>
2018-12-30 15:17:36 +0200

    fix main page, single slash error

--- middleware/removeSlash.js
+++ middleware/removeSlash.js
@@ -1,7 +1,7 @@
 export default function({ route, redirect }) {
   let path = route.path;
   let lastPathElementIndex = path.length - 1;
-  if (path[lastPathElementIndex] === '/') {
+  if (path.length > 1 && path[lastPathElementIndex] === '/') {
     return redirect(301, path.slice(0, lastPathElementIndex));
   }
 }
