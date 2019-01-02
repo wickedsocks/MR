399610db0dd1f32c84dcb8cb473bba6c64a56e17
wickedsocks <mykhailovpm@gmail.com>
2019-01-03 00:28:39 +0200

    change new date request with variable

--- server/api/sitemap_routes.js
+++ server/api/sitemap_routes.js
@@ -8,9 +8,10 @@ const router = Router();
 router.post('/generate-sitemap', (req, res) => {
   const { product, category } = req.body;
   const promiseAll = [];
+  const dateISO = new Date().toISOString();
   promiseAll.push(Product.find({}));
   promiseAll.push(Category.find({}));
-  const initialRoute = {url: '/', priority: 1, lastmodISO: new Date().toISOString()};
+  const initialRoute = {url: '/', priority: 1, lastmodISO: dateISO };
   Promise.all(promiseAll).then(
     result => {
       let products = result[0];
@@ -20,7 +21,7 @@ router.post('/generate-sitemap', (req, res) => {
           url: `${product}/${item.url}`,
           changefreq: 'daily',
           priority: 0.6,
-          lastmodISO: new Date().toISOString()
+          lastmodISO: dateISO
         };
       });
       productSitemap.push(initialRoute);
@@ -29,7 +30,7 @@ router.post('/generate-sitemap', (req, res) => {
           url: `${category}/${item.url}`,
           changefreq: 'daily',
           priority: 0.8,
-          lastmodISO: new Date().toISOString()
+          lastmodISO: dateISO
         };
       });
       let concatedSitemap = productSitemap.concat(categorySitemap);
