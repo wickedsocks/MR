6172a44773e92de8f38c1fcd9003804dc162e3bf
wickedsocks <mykhailovpm@gmail.com>
2019-01-19 20:10:41 +0200

    breadcrumbs test

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,14 +1,15 @@
 <template>
   <section>
     <ul  class="d-flex flex-wrap" itemscope itemtype="http://schema.org/BreadcrumbList">
-      <li itemscope itemtype="http://schema.org/ListItem">
+      <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
         <nuxt-link class="category-link cl6 hov-cl1" to="/" itemprop="item"><span itemprop="name">Главная</span></nuxt-link>
         <span> / </span> 
+        <meta itemprop="position" content="1"/>
       </li>
-      <li v-for="(link, index) in links" :key="index" itemscope itemtype="http://schema.org/ListItem"
-      itemprop="position">
+      <li itemprop="itemListElement" v-for="(link, index) in links" :key="index" itemscope itemtype="http://schema.org/ListItem">
         <nuxt-link class="category-link cl6 hov-cl1" :to="`/categories/${link.url}`" itemprop="item"> <span itemprop="name">{{link.title }}</span></nuxt-link>
         <span v-if="links.length - 1 != index"> / </span> 
+        <meta itemprop="position" :content="index + 2"/>
       </li>
     </ul>
   </section>
