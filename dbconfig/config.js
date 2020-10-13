4fcc29869bfa0fcf188e500c32adb6cf381c8696
Paul Mykhailov <pmykhailov@livegenic.com>
2020-10-13 19:47:57 +0300

    test

--- dbconfig/config.js
+++ dbconfig/config.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV == 'test' || process.env.NODE_ENV == 'development') {
   const config = require('./../config.json');
   const envConfig = config[process.env.NODE_ENV];
+  console.log('envConfig ', envConfig);
   Object.keys(envConfig).forEach((key) => {
     process.env[key] = envConfig[key];
   });
