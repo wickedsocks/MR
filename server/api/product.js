57f23300f307f05beb1b82844593f11043efabca
Paul Mykhailov <pmykhailov@livegenic.com>
2020-10-23 03:07:12 +0300

    sort by heifgh

--- server/api/product.js
+++ server/api/product.js
@@ -88,7 +88,7 @@ router.post('/pagination/products', async (req, res) => {
   Product.find({})
     .skip(skip)
     .limit(limit)
-    .sort('title')
+    .sort('productProperties.height')
     .then(
       success => {
         success = success.map(item => {
